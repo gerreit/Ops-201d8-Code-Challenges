14c9e72a1af925ace4c7c9bad2eaccd47861a454
Gerald <greit2022@gmail.com>
2023-04-26 21:02:40 +0000

    script for login history

---
+++ loginhistory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Script:                     Print login history                   
+# Author:                     Gerald                  
+# Date of latest revision:    4/26/2023  
+# Purpose:                    Shows the login history of computer and text that "This is the login history"
+
+# Basic function
+print_something () {
+  last
+}
+print_something
+print_something
+print_something
+echo
+echo "This is the login history"
