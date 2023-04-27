056739506da689433f00460aa5d29fd3f719c6e1
Gerald <greit2022@gmail.com>
2023-04-27 21:37:49 +0000

    last mintue changes. Forgot to submit yesterday so I just decided to make a few changes before submission

--- loginhistory.sh
+++ loginhistory.sh
@@ -5,7 +5,19 @@
 # Date of latest revision:    4/26/2023  
 # Purpose:                    Shows the login history of computer and text that "This is the login history"
 
+#Declariton of Variables
+
+
+
 # Basic function
+
+#history () {
+  #last -a
+  #echo "This is login history"
+}
+
+
+#Main
 history () {
   last -a
   echo
