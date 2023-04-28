6abf360a9068b7124b662a2479a88c938aad3a3b
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-04-27 20:07:36 -0400

    Update arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Script Name:               Arrays
+# Script Name:               Make directories in .txt files in those directories
 # Author:                    Gerald
 # Date of latest revision:   4/27/2023
-# Purpose:                  Arrays
+# Purpose:                   Make directories and in those directories put .txt files in each one
 
 
 #Declaration of variables:
@@ -19,17 +19,13 @@
 # dir=("dir1" "dir2" "dir3" "dir4")
 dir=(dir1 dir2 dir3 dir4)
 
-#echo ${dir[0]}
-#echo ${dir[1]}
-#echo ${dir[2]} 
-#echo ${dir[3]}
+
+# in the loop below it executes all the commands under do in order from top to bottom. After it hits the bottom it loops back to the top. the portion on line 24 the $ refers to the array (in this case dir as specified earlier) to and to do the insturctions [x] (or in this case of @ as many times until its out of elements in the array) many times until done. If i put 2 there it would only do 2 of the 4 elements so it would two directories and two .txt files 
 for i in ${dir[@]} 
 do
     mkdir "$i"
     touch "$i/example.txt"
-    #mkdir dir2
-    #ouch cd example.txt
 done
-#touch $dir "professionalcoder.txt"
+
 
 #End
