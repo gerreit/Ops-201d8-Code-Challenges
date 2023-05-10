4a30e1bd0ed6ad848187e60cc8efeff42e36730f
Gerald Reitmeyer <129210349+gerreit@users.noreply.github.com>
2023-05-10 16:06:36 -0700

    robo copy code

---
+++ Code challenge 8.ps1
@@ -0,0 +1,17 @@
+﻿
+# Script Name:               Robocopy script
+# Author:                    Gerald
+# Date of latest revision:   5/10/2023
+# Purpose:                   backup files to seperate location
+
+#Declaration of variables:
+
+
+#Declaration of functions:
+
+#Main
+
+robocopy C:\Users\J.thompson\Desktop\WorkFiles C:\Users\J.thompson\Documents /E
+
+#End
+
